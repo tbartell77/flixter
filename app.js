b2713255f663627fcb0b4ab98926d11fbd6f030e
tbartell77 <tyler.bartell7@gmail.com>
2025-02-19 11:40:56 -0600

    6th commit

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
-const loaderWrapper = document.querySelector(".loader__wrapper")
+async function main(searchTerm) {
+  const movies = await fetch(
+    `https://www.omdbapi.com/?s=${searchTerm}&apikey=6719cb69`
+  );
+  const moviesData = await movies.json();
+  const moviesListEl = document.querySelector(".movies__list");
 
-function handleLoader() {
-    loaderWrapper.innerHTML='<i class="fa-solid fa-spinner"></i>'
-    loaderWrapper.classList += ' loader__clicked'
+  moviesListEl.innerHTML = moviesData.Search.map((movie) =>
+    moviesHTML(movie)
+  ).join("");
 }
 
+function moviesHTML(movie) {
+  return `<div class="movie-card">
+            <div class="movie-card__container">
+              <img src= ${movie.Poster} alt=""/>
+              <h3>${movie.Title}</h3>
+              <p><b>${movie.Year}</b></p>
+            </div>
+          </div>`;
+}
+
+function searchMovies(event) {
+  main(event.target.value);
+  console.log(event.target.value);
+}
+
+const loaderWrapper = document.querySelector(".loader__wrapper");
+
+function handleLoader() {
+  loaderWrapper.innerHTML = '<i class="fa-solid fa-spinner"></i>';
+  loaderWrapper.classList += " loader__clicked";
+}
